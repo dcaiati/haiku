89b5cca1547fff285ee5e0cfd5d50e1840778af2
David Caiati <davidcaiati@Davids-MacBook-Pro-2.local>
2016-12-09 20:14:24 -0500

    added form to upload a file.

--- haiku.php
+++ haiku.php
@@ -1,3 +1,41 @@
+<html>
+<head>
+<title>David Caiati</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<form action="/art/haiku.php" method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<p>
+<?php 
+
+if (is_set($_POST['submit'])) {
+
+    if ($_FILES["fileToUpload"]["size"] > 5000) {
+        echo "Sorry, your file is too large.";
+
+    } else {
+
+        $file = $_FILES["fileToUpload"]["tmp_name"])
+        $h = new Haiku($file);
+        $haiku = $h->getHaiku();
+        echo $haiku;
+    }
+}
+
+?>
+</body>
+</html>
 <?php
 /*
 
